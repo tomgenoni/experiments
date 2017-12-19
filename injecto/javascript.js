0d10789289ed4c6c70fabff44232cc37a380d3c3
Tom Genoni <tom@atomeye.com>
2017-12-19 14:51:48 -0800

    More els

--- injecto/javascript.js
+++ injecto/javascript.js
@@ -60,7 +60,7 @@ var colorMap = [
     }
 ]
 
-var els = ['html','body','div','span','strong','form','h1','h2','h3','h4','h5','h6','a'];
+var els = ['html','body','div','span','strong','form','h1','h2','h3','h4','h5','h6','a','table','tr','td','figure','nav','ol','ul','li'];
 
 // Functions
 
