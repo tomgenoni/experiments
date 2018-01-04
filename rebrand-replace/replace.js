bbe55382fbdbbe3ddc4d0decadd48136725794c2
Tom Genoni <tom@atomeye.com>
2018-01-04 11:50:56 -0800

    Remove styles

--- rebrand-replace/replace.js
+++ rebrand-replace/replace.js
@@ -2,23 +2,24 @@ const color   = require('./json/color.json');
 const replace = require('replace');
 
 color.forEach(function(entry){
-    entry.sass.old.forEach(function(oldValue){
 
-        replace({
-          regex: "\\" + oldValue + "(?=;| )",
-          replacement: entry.sass.new,
-          paths: [
-              '/Users/tom/Sites/website/thumbprint/globals'
-          ],
-          excludeList: 'exclude.txt',
-          include: '*.scss',
-          recursive: true,
-          count: true,
-          silent: false,
-        });
+    if (entry.sass.old) {
 
-    })
-})
+        entry.sass.old.forEach(function(oldValue){
 
+            replace({
+                regex: "\\" + oldValue,
+                replacement: entry.sass.new,
+                paths: [
+                    '/Users/tom/Sites/website/thumbprint/globals'
+                ],
+                excludeList: 'exclude.txt',
+                include: '*.scss',
+                recursive: true,
+                count: true,
+                silent: false,
+            });
 
-//searchText.replace(/\$foo(?=;| )/gm, "\\$bar")
+        })
+    }
+});
--- rebrand-replace/styles/README.md
+++
@@ -1,9 +0,0 @@
-Global styles
-=============
-
-This directory contains global style rules that are included on every Thumbprint page.
-There are mostly the base elements of the UI system, like colors, typography, and icons.
-
-If in doubt, err on the side of not adding any new files to this directory. Remember, components
-can be CSS-only. If you just want to add some styles without any JavaScript, but they're not
-generic enough to go on every page, make a CSS-only component in thumbprint/components.
