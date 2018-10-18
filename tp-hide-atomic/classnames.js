e5bb22a9fa2a32a5a1a895dab136c2033d00f4a4
Tom Genoni <tom@thumbtack.com>
2018-10-18 12:20:46 -0700

    Update

--- tp-hide-atomic/classnames.js
+++ tp-hide-atomic/classnames.js
@@ -4,6 +4,16 @@ boxes.forEach(function(box, i){
     let boxClasses = box.className.split(" ");
     boxClasses.shift();
 
+    // const boxWrap = box.parentElement;
+    // console.log(boxWrap);
+
+    boxClasses.forEach(function(item){
+        if (item.includes("db")) {
+            const boxWrap = box.parentElement;
+            boxWrap.classList.add("box-display");
+        }
+    });
+
     const code = box.parentElement.previousElementSibling;
     const codeClasses = boxClasses.join(' ');
     code.innerHTML = codeClasses;
