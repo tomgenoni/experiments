802cbc7196dd6486a672ad686bf3971af31e5a28
Tom Genoni <tom@thumbtack.com>
2018-12-19 11:39:57 -0800

    Testing

---
+++ sidenav-show/local.js
@@ -0,0 +1,11 @@
+const toggle = document.querySelector("#toggle");
+const close = document.querySelector("#close");
+const nav = document.querySelector("#nav");
+
+toggle.addEventListener("click", function(){
+    nav.classList.add("is-active")
+})
+
+close.addEventListener("click", function(){
+    nav.classList.remove("is-active")
+})
