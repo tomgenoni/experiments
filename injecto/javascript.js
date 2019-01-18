b5ce44f50a1c0152d3df5ad12ea977d0c8d41033
Tom Genoni <tom@thumbtack.com>
2019-01-18 12:00:41 -0800

    Add styling for email

--- injecto/javascript.js
+++ injecto/javascript.js
@@ -1,2 +1,38 @@
 console.log("injecto: js loaded");
+
+const body = document.querySelector("body");
+const content = document.querySelector("[data-algolia='content");
+const aria = document.querySelectorAll("[aria-hidden='true");
+const h1 = document.querySelector("h1");
+const footer = document.createElement("footer");
+footer.innerHTML = `
+<div class="mt3 tp-body-3">
+    <div>
+        <a class="black-300 underline" href="https://thumbprint.design">thumbprint.design</a>
+    </div>
+    <div>
+        <a class="black-300 underline" href="https://teamsanfrancisco.slack.com/messages/C0GKYQVTR">#design-systems</a>
+    </div>
+    <div>
+        <a class="black-300 underline" href="https://thumbprint.design/notes/2018-11-06/">View online</a>
+    </div>
+</div>`;
+
+// Remove things
+aria.forEach(function(el) {
+    el.remove();
+});
+h1.remove();
+
+// Replace body with content
+body.innerHTML = content.innerHTML;
+
+// Remove all classes on elements
+const els = document.querySelectorAll("*");
+els.forEach(function(el) {
+    el.classList = "";
+});
+
+body.appendChild(footer);
+
 console.log("injecto: js complete");
